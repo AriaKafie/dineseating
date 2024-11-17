a11e36fa5539e01ab6bb6672efdf4b39ab0c7559
Aria Kafie <aria.kafie@verizon.net>
2024-11-17 13:00:47 -0800

    update

--- main.cpp
+++ main.cpp
@@ -75,7 +75,7 @@ void *produce_vip(void *ptr)
     pthread_exit(SUCCESS);
 }
 
-void *consume(void *ptr)
+void *tx_consumer(void *ptr)
 {
     unsigned int consumed[RequestTypeN] = {};
     
@@ -110,14 +110,50 @@ void *consume(void *ptr)
     pthread_exit(SUCCESS);
 }
 
+void *r9_consumer(void *ptr)
+{
+    unsigned int consumed[RequestTypeN] = {};
+    
+    SharedData *sd = (SharedData*)ptr;
+
+    for (;;)
+    {
+        sem_wait(&sd->unconsumed);
+        pthread_mutex_lock(&sd->lock);
+
+        if (sd->total_consumed >= sd->max_requests)
+            break;
+        
+        RequestType rt = sd->requests.front();
+        sd->requests.pop();
+        sd->total_consumed++;
+
+        consumed[rt]++;
+        sd->in_request_queue[rt]--;
+
+        output_request_removed(Rev9, rt, consumed, sd->in_request_queue);
+
+        pthread_mutex_unlock(&sd->lock);
+        sem_post(&sd->consumed);
+        if (rt == VIPRoom)
+            sem_post(&sd->vip_consumed);
+
+        //sleep(100);
+    }
+    
+    sem_post(&sd->main_blocker);
+    pthread_exit(SUCCESS);
+}
+
 int main(int argc, char **argv)
 {
     SharedData s(100);
     pthread_t general_producer, vip_producer, tx, r9;
 
     pthread_create(&general_producer, NULL, &produce_general, &s);
-    pthread_create(&vip_producer, NULL, &produce_vip, &s);
-    pthread_create(&tx, NULL, &consume, &s);
+    pthread_create(&vip_producer,     NULL, &produce_vip,     &s);
+    pthread_create(&tx,               NULL, &tx_consumer,     &s);
+    pthread_create(&r9,               NULL, &r9_consumer,     &s);
     
     sem_wait(&s.main_blocker);
 

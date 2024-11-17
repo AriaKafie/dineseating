0157bf85686290b4734d5c005abb2ab88c189f33
Aria Kafie <aria.kafie@verizon.net>
2024-11-17 13:25:51 -0800

    templates

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ void sleep(int ms) {
     usleep(ms * US_PER_MS);
 }
 
-void *produce_general(void *ptr)
+template<RequestType T>
+void *producer(void *ptr)
 {
     SharedData *sd = (SharedData*)ptr;
 
@@ -21,6 +22,8 @@ void *produce_general(void *ptr)
         // sleep to simulate production
 
         sem_wait(&sd->consumed);
+        if (T == VIPRoom)
+            sem_wait(&sd->vip_consumed);
         pthread_mutex_lock(&sd->lock);
 
         if (sd->produced[GeneralTable] + sd->produced[VIPRoom] >= sd->max_requests)
@@ -30,11 +33,11 @@ void *produce_general(void *ptr)
             break;
         }
         
-        sd->requests.push(GeneralTable);
-        sd->produced[GeneralTable]++;
-        sd->in_request_queue[GeneralTable]++;
+        sd->requests.push(T);
+        sd->produced[T]++;
+        sd->in_request_queue[T]++;
         
-        output_request_added(GeneralTable, sd->produced, sd->in_request_queue);
+        output_request_added(T, sd->produced, sd->in_request_queue);
 
         pthread_mutex_unlock(&sd->lock);
         sem_post(&sd->unconsumed);
@@ -43,39 +46,8 @@ void *produce_general(void *ptr)
     pthread_exit(SUCCESS);
 }
 
-void *produce_vip(void *ptr)
-{
-    SharedData *sd = (SharedData*)ptr;
-
-    for (;;)
-    {
-        // sleep to simulate production
-
-        sem_wait(&sd->consumed);
-        sem_wait(&sd->vip_consumed);
-        pthread_mutex_lock(&sd->lock);
-
-        if (sd->produced[GeneralTable] + sd->produced[VIPRoom] >= sd->max_requests)
-        {
-            pthread_mutex_unlock(&sd->lock);
-            sem_post(&sd->unconsumed);
-            break;
-        }
-        
-        sd->requests.push(VIPRoom);
-        sd->produced[VIPRoom]++;
-        sd->in_request_queue[VIPRoom]++;
-        
-        output_request_added(VIPRoom, sd->produced, sd->in_request_queue);
-        
-        pthread_mutex_unlock(&sd->lock);
-        sem_post(&sd->unconsumed);
-    }
-    
-    pthread_exit(SUCCESS);
-}
-
-void *tx_consumer(void *ptr)
+template<ConsumerType T>
+void *consumer(void *ptr)
 {
     unsigned int consumed[RequestTypeN] = {};
     
@@ -96,42 +68,7 @@ void *tx_consumer(void *ptr)
         consumed[rt]++;
         sd->in_request_queue[rt]--;
 
-        output_request_removed(TX, rt, consumed, sd->in_request_queue);
-
-        pthread_mutex_unlock(&sd->lock);
-        sem_post(&sd->consumed);
-        if (rt == VIPRoom)
-            sem_post(&sd->vip_consumed);
-
-        //sleep(100);
-    }
-    
-    sem_post(&sd->main_blocker);
-    pthread_exit(SUCCESS);
-}
-
-void *r9_consumer(void *ptr)
-{
-    unsigned int consumed[RequestTypeN] = {};
-    
-    SharedData *sd = (SharedData*)ptr;
-
-    for (;;)
-    {
-        sem_wait(&sd->unconsumed);
-        pthread_mutex_lock(&sd->lock);
-
-        if (sd->total_consumed >= sd->max_requests)
-            break;
-        
-        RequestType rt = sd->requests.front();
-        sd->requests.pop();
-        sd->total_consumed++;
-
-        consumed[rt]++;
-        sd->in_request_queue[rt]--;
-
-        output_request_removed(Rev9, rt, consumed, sd->in_request_queue);
+        output_request_removed(T, rt, consumed, sd->in_request_queue);
 
         pthread_mutex_unlock(&sd->lock);
         sem_post(&sd->consumed);
@@ -150,12 +87,10 @@ int main(int argc, char **argv)
     SharedData s(100);
     pthread_t general_producer, vip_producer, tx, r9;
 
-    pthread_create(&general_producer, NULL, &produce_general, &s);
-    pthread_create(&vip_producer,     NULL, &produce_vip,     &s);
-    pthread_create(&tx,               NULL, &tx_consumer,     &s);
-    pthread_create(&r9,               NULL, &r9_consumer,     &s);
+    pthread_create(&general_producer, NULL, &producer<GeneralTable>, &s);
+    pthread_create(&vip_producer,     NULL, &producer<VIPRoom>,      &s);
+    pthread_create(&tx,               NULL, &consumer<TX>,           &s);
+    pthread_create(&r9,               NULL, &consumer<Rev9>,         &s);
     
     sem_wait(&s.main_blocker);
-
-    std::cout << "Done" << std::endl;
 }
